0c41eeb56e1f92dff16c5014e07afc5c9d98349e
Kirill Varnatsov <kirill.varnatsov@gmail.com>
2022-02-26 14:40:41 +0200

    added price plot/pagination & refractore stuff

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,10 +3,10 @@ import { useRecoilState } from 'recoil';
 import { Card, Form, Container, Row, Col, Pagination } from 'react-bootstrap'
 import './Main.css'
 import { items } from '../store/index'
-import { URL } from '../../credentials'
+// import { URL } from '../../credentials'
 // COMPONENTS
 import ItemModal from '../Modal/Modal'
-
+const URL = "http://127.0.0.1:8080"
 const parameters = {
   method: 'GET', // *GET, POST, PUT, DELETE, etc.
   mode: 'cors', // no-cors, *cors, same-origin
@@ -18,44 +18,33 @@ const parameters = {
   }
 }
 
+function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
+
 export default function Main() {
     const [products, setProducts] = useState({});
     const [modalShow, setModalShow] = useState(false);
-    const [modalData, setModalData] = useState({});
+    const [modalItem, setModalItem] = useState({});
     const [showItemCount, setShowItemCount] = useState(64);
     const [dates, setDates] = useState([])
+    const [pageCount, setPageCount] = useState(1)
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
+        if (Object.keys(products).length == 0)
+            setPageCount(Math.ceil(Object.keys(products).length/showItemCount))
         fetchProducts()
     }, [products]);
 
     useEffect(() => {
         fetchDates()
     }, [dates]);
-    // const orderProductsByName = async (date) => {
-    //     try {
-    //         const data = await fetch(URL + "/prices/" + date.replaceAll("'", ""), parameters)
-    //         const json = await data.json()
-    //         let normalized = dates
-    //         json.map(item => { 
-    //             let key = item[1]
-    //             if (item[3] == "selver") {
-    //                 key = `${item[0]}, ${item[1]}`
-    //             }
-    //             if (!(key in itemData)) {
-    //                 setItemData({ [normalized[key]] :  {"id": item[0], "name": item[1], "price": item[2], "shop": item[3], "discount": item[4]}})
-    //             }
-    //         });
-    //         console.log(Object.keys(itemData).length)
-    //     } catch (e){
-    //         if(e instanceof TypeError) {
-    //             console.log(e)
-    //         } else {
-    //             console.log(e)
-    //         }
-    //     }
-    // }
 
+    useEffect(() => {
+        if (Object.keys(products).length != 0)
+            setPageCount(Math.ceil(Object.keys(products).length/showItemCount))
+    }, [showItemCount]);
 
     const fetchDates = async () => {
         const response = await fetch(URL, parameters);
@@ -68,16 +57,14 @@ export default function Main() {
         setProducts(json)
     }
 
-    const openModal = (name) => {
-        setModalData({itemname: name})
+    const openModal = (name, id) => {
+        setModalItem({itemname: name, id: id})
         setModalShow(true)
     }
 
     const closeModal = () => {
         setModalShow(false)
     }
-
-
 
     return (
         <div>
@@ -128,17 +115,34 @@ export default function Main() {
                 </Form>
             </Container>
             <hr/>
-            <ItemModal  show={modalShow} data={modalData} close={closeModal}></ItemModal>
+            {modalShow ? <ItemModal  show={modalShow} item={modalItem} close={closeModal}></ItemModal> : null}
             <Container fluid className="d-flex flex-wrap justify-content-around mt-2" >
-                {Object.keys(products).slice(0, showItemCount).map((item, index) => (
-                    <Card onClick={() => openModal(item)} key={index} style={{ width: '14rem' }} className="mb-2 shadow-sm item-card">
+                {Object.keys(products).slice(showItemCount*(page-1), showItemCount * page).map((item, index) => (
+                    <Card onClick={() => openModal(products[item]["name"], products[item]["id"])} key={index} style={{ width: '14rem' }} className="mb-2 shadow-sm item-card">
                         <Card.Body>
-                        <Card.Title>{item}</Card.Title>
+                        <Card.Title>{products[item]["name"]}</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted text-capitalize">{products[item]['shop']}</Card.Subtitle>
                         </Card.Body>
                     </Card>
                 ))}
             </Container>
+            <Pagination className="justify-content-center mt-2">
+                <Pagination.First onClick={() => setPage(3)}/>
+                {page > 3 ? <Pagination.Prev onClick={() => setPage(page-1)}/> : <Pagination.Prev disabled/>}
+                {page-3 >= 3 ? <Pagination.Ellipsis disabled/> : null}
+                {range(page - 2, page + 2).map(i => {
+                    if (i - 2 >= 1 & i <= pageCount) {
+                        if (i == page) {
+                            return <Pagination.Item active onClick={() => setPage(i)} key={i}>{i-2}</Pagination.Item>
+                        } else {
+                            return <Pagination.Item onClick={() => setPage(i)} key={i}>{i-2}</Pagination.Item>
+                        }
+                    } 
+                })}
+                {pageCount-2 - page > 2 ? <Pagination.Ellipsis disabled/> : null}
+                {page <= pageCount-1 ? <Pagination.Next onClick={() => setPage(page+1)} /> : <Pagination.Next disabled/>}
+                <Pagination.Last onClick={() => setPage(pageCount)}/>
+            </Pagination>
         </div>
     )
 }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
+const URL = "http://127.0.0.1:8080"
+const parameters = {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*'
+    }
+  }
+
 export class ItemModal extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            plotData: []
+        }
+    }
+
+    
+    componentDidMount() {
+        this.fetchProductPrice(this.props.item.id, this.props.item.itemname)
+    }
+
+    async fetchProductPrice(id, name) {
+        const response = await fetch(URL + `/products/${name}/${id}`, parameters);
+        const json = await response.json()
+        const data = {plotData: json[name].map((index, item) => ({y: index, x: item}))}
+        this.renderChart(data.plotData)
+    }
+
+    renderChart(data) {
+        new Chart("chart", {
+            type: "scatter",
+            data: {
+                datasets: [{
+                pointRadius: 4,
+                pointBackgroundColor: "rgba(0,0,255,1)",
+                data: data
+                }]
+            },
+            scales: {
+                y: {
+                    max: 80,
+                    min: 20
+                }
+            }
+        });
     }
 
     render() {
         return (
             <Modal fullscreen={true} show={this.props.show} onHide={this.props.close}>
                 <Modal.Header closeButton>
-                    <Modal.Title>{this.props.data.itemname}</Modal.Title>
+                    <Modal.Title>{this.props.item.itemname}</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <p>Modal body text goes here.</p>
+                    <canvas id="chart"></canvas>
                 </Modal.Body>
 
                 <Modal.Footer>
